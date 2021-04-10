6704a1d552c4b35be082f2f228fb873f6a9cb35a
Catalin Postelnicu <catalinp2004@gmail.com>
2021-04-10 23:46:45 +0300

    minor style tweaks

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Mucca</title>
+    <title>Mucca - Playfully Committed</title>
     <meta name="description" content="Materiale publicitare si de vizibilitate.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
