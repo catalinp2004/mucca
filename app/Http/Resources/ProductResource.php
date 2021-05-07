34bb0862aeee1c9679daedbceeca3e325b4dd6ca
Catalin Postelnicu <catalinp2004@gmail.com>
2021-05-07 12:45:12 +0300

    added placeholder for products without image

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -16,13 +16,14 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         $subcategory = $this->subcategories->first();
+        $filename = ($this->images->count() > 0) ? Storage::url('img/products/'.$this->folder_name.'/'.$this->images->shuffle()->first()['filename']) : asset('img/mucca_placeholder.jpg');
         return [
             'id' => $this->id,
             'slug' => $this->slug,
             'product_code' => $this->product_code,
             'name' => $this->name,
             'description' => $this->description,
-            'filename' => Storage::url('img/products/'.$this->folder_name.'/'.$this->images->shuffle()->first()['filename']),
+            'filename' => $filename,
             'first_subcategory' => $subcategory,
             'first_category' => $subcategory->category,
         ];
