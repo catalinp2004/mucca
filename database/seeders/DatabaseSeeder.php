276602f8dfc30df4374cebfedc0d111ef9e21585
paulgungureanu <50268361+paulgungureanu@users.noreply.github.com>
2021-05-07 16:59:22 +0300

    created script for xml with prodinfo_RO

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -67,71 +67,129 @@ class DatabaseSeeder extends Seeder
 
         ];
         $cats = [
-            ['Articole pentru băuturi', 41],
-            ['Accesorii de birou', 64],
-            ['Portofolii', 64],
-            ['Ustensile de bucătărie', 40],
-            ['Echipament de ploaie', 67],
-            ['Accesorii pentru mașină', 50],
-            ['Accesorii de călătorie', 69],
-            ['Accesorii telefoane', 13],
-            ['Sport&amp;Sănătate', 70],
-            ['Brelocuri', 76],
-            ['Notesuri', 19],
-            ['Rucsacuri&amp;Genți business', 32],
-            ['Home &amp; Living', 80],
-            ['Încărcătoare', 9],
-            ['Îngrijire personală', 79],
-            ['Obiecte pentru bar', 79],
-            ['Elementele esențiale pentru îngrijire', 79],
-            ['Desen', 49],
-            ['Jocuri', 49],
-            ['Jucării din pluș', 49],
-            ['USB-uri', 10],
-            ['Audio și sunet', 12],
-            ['Ochelari', 52],
-            ['Scris', 17],
-            ['Prim ajutor', 77],
-            ['Anti stres/Bomboane', 48],
-            ['Exterior', 56],
-            ['Produse pentru plajă', 56],
-            ['Rucsacuri&amp;Exterior', 33],
-            ['Sacoșe pentru cumpărături', 78],
-            ['Căciuli/șepci/accesorii', 25],
-            ['Umbrele', 47],
-            ['Umbrele de furtună', 47],
-            ['Unelte&Lanterne', 81],
-        ];
-        $bautura = [
-            ['Seturi&Altele', 41],
-            ['Pai', 41],
-            ['Căni&amp;Pahare', 38],
-            ['Termosuri', 37],
-            ['Sticle multifuncționale', 37],
-            ['Sticle pentru apă', 36],
-            ['Sticle pentru sport', 36],
+            ["WINE ACCESORIES", 41],
+            ["DESK ITEMS", 64],
+            ["WRITING / PAINTING CASE", 64],
+            ["LANYARDS", 64],
+            ["WEATHER STATIONS", 64],
+            ["PORTFOLIO / CONFERENCE FOLDER", 64],
+            ["CLOCKS", 64],
+            ["BUSINESS CARD HOLDERS", 64],
+            ["HIGHLIGHTERS &amp; MARKERS", 64],
+            ["LASER POINTER", 64],
+            ["KITCHEN ACCESORIES", 40],
+            ["BOARD", 40],
+            ["SCARFS", 67],
+            ["CLOTHES ACCESORIES", 67],
+            ["HAT",67],
+            ["CAR ACCESSORIES", 50],
+            ["TRAVEL ACCESORIES", 69],
+            ["WAIST BAG", 69],
+            ["TRAVEL BAG", 69],
+            ["SUITCASES &amp; TROLLEYS", 69],
+            ["COMPUTER ACCESORIES", 16],
+            ["CAMERA", 16],
+            ["SMOKE DETECTOR", 16],
+            ["TELEPHONE / ACCESSORIES", 13],
+            ["CD ACCESSORIES", 41],
+            ["HEALTH", 70],
+            ["FITNESS", 70],
+            ["FITNESS ITEMS", 70],
+            ["PEDOMETER", 70],
+            ["BIKES &amp; SCOOTERS", 57],
+            ["KEY RINGS / CHAINS / HOLDERS / CASE", 76],
+            ["CALCULATOR", 20],
+            ["CUPS", 38],
+            ["GLASSES", 38],
+            ["NOTEBOOKS / NOTEPADS", 19],
+            ["WATCHES", 11],
+            ["PENCIL", 18],
+            ["DOCUMENT / CONFERENCE BAG", 68],
+            ["LAPTOP BAG", 32],
+            ["BBQ SETS", 59],
+            ["Sportbags", 31],
+            ["GARDEN", 58],
+            ["H&amp;L", 80],
+            ["DECORATION", 80],
+            ["AMBIENTE", 80],
+            ["BATHROOM ACCESORIES", 80],
+            ["POWERBANKS", 9],
+            ["LIPBALM", 79],
+            ["COSMETIC / BEAUTY CASE", 79],
+            ["LADY'S STYLING", 79],
+            ["MANICURE SET", 79],
+            ["FAN", 79],
+            ["SEWING SET", 79],
+            ["SHOE POLISH SET", 79],
+            ["SUNSCREEN", 79],
+            ["PERSONALCARE, WALLET", 79],
+            ["BEACH", 79],
+            ["CHILDREN / FUNNY ACCESORIES", 49],
+            ["GAMES", 49],
+            ["STUFFED ANIMALS", 49],
+            ["PLUSH", 49],
+            ["PLANT", 55],
+            ["SPEAKERS", 12],
+            ["MP3 PLAYER", 12],
+            ["SUNGLASSES", 52],
+            ["PEN", 17],
+            ["ROLLER", 17],
+            ["WALLETS", 45],
+            ["ANTISTRESS", 48],
+            ["THERMO", 37],
+            ["CAMPING &amp; PICNIC", 56],
+            ["RELAX", 56],
+            ["BOOKS ACCESSORIES", 56],
+            ["PET CARE", 56],
+            ["Backpack/rucksack", 33],
+            ["CITY BAG / BACKPACK", 33],
+            ["SHOPPING BAG", 78],
+            ["DUFFLE BAG", 34],
+            ["CAPS &amp; HATS", 25],
+            ["GIFT SET", 61],
+            ["DRINKING BOTTLE", 36],
+            ["RAINCOAT", 47],
+            ["UMBRELLA", 47],
+            ["SURVIVAL", 81],
+            ["BRICOLAGE", 81],
+            ["BATTERY", 81],
+            ["TORCHES &amp; FLASHLIGHTS", 81],
+            ["LIGHTERS", 81],
 
         ];
 
-        if (file_exists('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_ro_v1.1.xml')) {
-            $xml = simplexml_load_file('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_ro_v1.1.xml');
+//        if (file_exists('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_RO.xml')) {
+//            $xml = simplexml_load_file('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_RO.xml');
+//            $array = [];
+//            foreach ($xml->PRODUCT as $product){
+//                    $a = $product->CATEGORY_LEVEL_3.PHP_EOL;
+//                    $array[]=$a;
+//
+//
+//            }
+//            $array= array_unique($array);
+//
+//            foreach ($array as $item){
+//                print_r($item);
+//            }
+//
+//        } else {
+//            exit('Failed to open test.xml.');
+//        }
+
+
+        if (file_exists('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_RO.xml')) {
+            $xml = simplexml_load_file('C:\Proiecte\Catalin\mucca\database\seeders\prodinfo_RO.xml');
                 foreach ($xml->PRODUCT as $product){
                     $subcategory_id = null;
-                    if ($product->CATEGORY_LEVEL_3 == 'Băutură') {
-                        foreach ($bautura as $baut) {
-                            if ( $baut[0] == $product->CATEGORY_LEVEL_4) {
-                                $subcategory_id = $baut[1];
-                                break;
-                            }
-                        }
-                    } else {
-                        foreach ($cats as $cat){
-                            if ($cat[0] == $product->CATEGORY_LEVEL_3) {
-                                $subcategory_id = $cat[1];
-                                break;
-                            }
+
+                    foreach ($cats as $cat){
+                        if ($cat[0] == $product->CATEGORY_LEVEL_3) {
+                            $subcategory_id = $cat[1];
+                            break;
                         }
                     }
+
                     if ($subcategory_id !=null) {
                         $exists = Product::where('product_code', $product->PRODUCT_BASE_NUMBER)->first();
                         if ($exists == null) {
@@ -145,6 +203,7 @@ class DatabaseSeeder extends Seeder
                             ];
                             $db_product = new Product();
                             $db_product->fill($valid);
+                            sleep(1);
                             $db_product->save();
                             DB::table('product_subcategory')->insert([
                                 'product_id' => $db_product->id,
@@ -178,7 +237,19 @@ class DatabaseSeeder extends Seeder
 
                         /* Check for 404 (file not found). */
                         $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-                        if($httpCode != 404 && $url != '') {
+                        if ($httpCode == 403){
+                            curl_close($handle);
+                            $url = explode('.jpg', $url)[0] . 'a.jpg';
+                            $handle = curl_init($url);
+                            curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+
+                            /* Get the HTML or whatever is linked in $url. */
+                            $response = curl_exec($handle);
+
+                            /* Check for 404 (file not found). */
+                            $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+                        }
+                        if($httpCode != 404 && $httpCode != 403 && $url != '') {
                             $contents = file_get_contents($url);
                             $name = substr($url, strrpos($url, '/') + 1);
                             Storage::put('public/img/products/'.$slug_date.'/'.$name, $contents);
